7e1e17f7daf7d3a82131fd1185b66cf3928cde26
OLO-spec <chilledoutrobot96@gmail.com>
2026-04-20 22:07:45 -0700

    Did HW1,2,3

--- start/CH02/HelloWorld3.py
+++ start/CH02/HelloWorld3.py
@@ -1,3 +1,14 @@
 #!/usr/bin/env python3
 # A more complex "Hello World" script in python with Inputs
-# Created 
+# Created 4/20/26
+
+#Ask user for their name
+user_name = input("What is your name? ")
+
+#Print hello and their name
+print("Hello {0}".format(user_name))
+print(f"Hello {user_name}")
+print("Hello "+ user_name)
+print("Hello", user_name)
+message = "Hello {0}".format(user_name)
+print(message)
