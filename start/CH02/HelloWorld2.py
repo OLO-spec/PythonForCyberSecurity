d1afe041c667271ca2033c673c3ebff4b8a09307
OLO-spec <chilledoutrobot96@gmail.com>
2026-04-20 21:53:02 -0700

    Started HW1 and HW2

--- start/CH02/HelloWorld2.py
+++ start/CH02/HelloWorld2.py
@@ -5,3 +5,11 @@
 # Suggestion, build out 1 line at a time
 # Once multiple print statemetns exist, put a breakpoint at first print line
 # Then walk through as an example of "debugging"
+
+#4/20/26
+
+#Ask user for their name
+user_name = input("What is your name? ")
+
+#Print hello and their name
+print("Hello {0}".format(user_name))
