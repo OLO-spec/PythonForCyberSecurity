8ba1824cc9266ece1dfb05b0c5fba55420edfb67
OLO-spec <chilledoutrobot96@gmail.com>
2026-04-28 13:51:19 -0700

    chpater 4. read and write

--- start/CH04/ReadFile.py
+++ start/CH04/ReadFile.py
@@ -1,3 +1,12 @@
 #!/usr/bin/env python3
 # Sample script that reads from a file
-# By 
+# By D - 4/28/2026
+import os
+#open file for writing
+dir_path =os.path.dirname(os.path.realpath(__file__))
+f=open(dir_path + "/newtextfile.txt", "r")
+#read the file and print to screen
+contents = f.read()
+
+#closing the file
+f.close()
--- start/CH04/WriteFile.py
+++ start/CH04/WriteFile.py
@@ -1,3 +1,15 @@
 #!/usr/bin/env python3
 # Sample script that writes to a file
-# By 
+# By D - 4/28/2026
+import os
+#open file for writing
+dir_path =os.path.dirname(os.path.realpath(__file__))
+f=open(dir_path + "/newtextfile.txt", "w")
+
+#write to file
+print ("hello world")
+f.write("Hello World, have a good day\n")
+f.write("Have a good day")
+
+#closing the file
+f.close()
